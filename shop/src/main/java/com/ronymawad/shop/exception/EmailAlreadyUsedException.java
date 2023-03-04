a65a00ab01e87b18b79b685634df96e41f5ad144
Rony <ronymawad@gmail.com>
2023-03-04 18:07:03 -0500

    Revert "First commit after repository division and adding secrets"
    This reverts commit 8642e50966c01e286e5cb7a3d041ed8f44cbf11f.

--- shop/src/main/java/com/ronymawad/shop/exception/EmailAlreadyUsedException.java
+++
@@ -1,7 +0,0 @@
-package com.ronymawad.shop.exception;
-
-public class EmailAlreadyUsedException extends Exception {
-    public EmailAlreadyUsedException(String email){
-        super(email +" is already used for another account.");
-    }
-}
--- shop/src/main/java/com/ronymawad/shop/exception/UserNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.ronymawad.shop.exception;
-
-public class UserNotFound extends Exception {
-    public UserNotFound(String email) {
-        super("User with email : " +email +" is not found!");
-    }
-}
